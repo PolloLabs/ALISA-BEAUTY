13204a1da61067941e0cdff0811587a34dec7f30
Marcos Kules <kulesmarcos@gmail.com>
2026-09-15 20:57:05 -0300

    Add FaviconUpload component import in SalonSettings

--- src/pages/settings/SalonSettings.tsx
+++ src/pages/settings/SalonSettings.tsx
@@ -35,6 +35,7 @@ import { toast } from 'react-hot-toast';
 import { useSalon } from '@/hooks/useSalon';
 import { usePlan } from '@/hooks/usePlan';
 import { ShareBookingLink } from '@/components/ShareBookingLink';
+import { FaviconUpload } from '@/components/ui/FaviconUpload';
 import { BusinessType } from '@/types';
 import { cn, safeStorageGet, resizeImageToMax256 } from '@/lib/utils';
 
